9a15d5b0101e3a84c7b27fa1bdcc4ce4883b9757
Insori <s2110@e-mirim.hs.kr>
2023-08-24 14:31:56 +0900

    try catch문 활용한 예외처리

--- ExceptionPrac/Program.cs
+++ ExceptionPrac/Program.cs
@@ -12,15 +12,20 @@ namespace ExceptionPrac
         {
             string[] array = { "가", "나" };
             Console.Write("숫자를 입력하세요: ");
-            int input = int.Parse(Console.ReadLine());
 
-            if(input >= 0 && input < array.Length)
+            try
             {
+                int input = int.Parse(Console.ReadLine());
                 Console.WriteLine("입력한 위치의 값은 '" + array[input] + " '입니다.");
-            }
-            else
+            } catch (IndexOutOfRangeException e) {
+                Console.WriteLine("인덱스 범위를 벗어났습니다.");
+            } catch (FormatException e) {
+                Console.WriteLine("숫자를 입력하세요.");
+            } catch (Exception e)
             {
-                Console.WriteLine("올바른 인덱스를 입력해주세요.");
+                Console.WriteLine(e.GetType());
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
             }
         }
     }
